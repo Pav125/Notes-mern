69d0f826277e7ff70a61eedf16683d10ded15b8b
Pav125 <devipavan824@gmail.com>
2024-05-23 23:58:49 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: ['https://mymemo.vercel.app'], // 'https://mymemo.vercel.app','http://localhost:3000'
+        origin: ['https://mymemo.vercel.app','http://localhost:3000'], // 'https://mymemo.vercel.app','http://localhost:3000'
         // methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
         // allowedHeaders: ['Content-Type', 'Authorization']
