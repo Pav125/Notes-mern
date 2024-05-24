fd0689cfb60cb8f1924a986aa3c453b8a71b14bf
Pav125 <devipavan824@gmail.com>
2024-05-24 11:43:34 +0530

    ready to deploy

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
                 <>
                     <nav className="navbar navbar-expand-lg navbar-custom">
                         <div className="container-fluid ms-3 mr-3">
-                            <a className="navbar-brand" href="/">Notes</a>
+                            <a className="navbar-brand" href="/">MyMemo</a>
                             <div className="" id="navbarNav">
                                 <ul className="navbar-nav ms-auto">
                                     <li className="nav-item">
